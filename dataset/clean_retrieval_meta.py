e95093ba6494d53b6fce8bb7f25a6d080867c7cc
ymkim1019 <ymkim1019@gmail.com>
2017-05-20 14:31:55 +0900

    Revert "metafile cleaned.."
    This reverts commit 418bf108a511eb769fa6ac9793ca5de1074b54b3.

--- dataset/clean_retrieval_meta.py
+++ dataset/clean_retrieval_meta.py
@@ -1,47 +1,27 @@
 import json
-from os.path import join
-from PIL import Image
-import numpy as np
-
-
-def load_image(path):
-    try:
-        im = Image.open(path)
-        if im.format == 'GIF' or im.format == 'PNG':
-            im = im.convert('RGB')
-        elif im.format == 'PNG':
-            im.load()
-            background = Image.new("RGB", im.size, (255, 255, 255))
-            background.paste(im, mask=im.split()[-1])  # 3 is the alpha channel
-            im = background
-
-        arr = np.asarray(im, dtype="int32")
-        return arr
-    except Exception as e:
-        print('img load error.. :', path)
-        return None
-
+from os import listdir, getcwd
+from os.path import isfile, join, splitext
 
 def main(retrieval_meta_fname, img_dir):
+    items = set()
+    onlyfiles = [f for f in listdir(img_dir) if isfile(join(img_dir, f))]
+    for p in onlyfiles:
+        filename, _ = splitext(p)
+        items.add(int(filename))
+
     with open(retrieval_meta_fname) as f:
         js = json.loads(f.read())
-        new_js = []
-        n = len(js)
-        for i, each in enumerate(js):
-            if i % 100 == 0:
-                print(i+1, '/', n);
-            if load_image(join(img_dir, "%09d" % int(each['photo']) + '.jpeg')) is not None:
-                new_js += each
+        new_js = [obj for obj in js if int(obj['photo']) in items]
 
-    with open(retrieval_meta_fname, 'w') as f:
+    filename, extension = splitext(retrieval_meta_fname)
+    p = filename + '_cleaned' + extension
+    with open(p, 'w') as f:
         json.dump(new_js, f)
 
-
 if __name__ == '__main__':
-    category_list = ['pants', 'outerwear', 'bags', 'belts', 'dresses', 'eyewear', 'footwear', 'hats', 'leggings', 'skirts', 'tops']
-    # category_list = ['belts']
+    category_list = ['pants', 'outwear', 'bags', 'belts', 'dresses', 'eyewear', 'footwear', 'hats', 'leggings', 'skirts', 'tops']
     for category in category_list:
-        retrieval_meta_fname = "meta/meta/json/retrieval_" + category + "_cleaned.json"
-        img_dir = "images/" + category
+        retrieval_meta_fname = "meta\\meta\\json\\retrieval_" + category + ".json"
+        img_dir = "images\\" + category
         main(retrieval_meta_fname, img_dir)
         print(category + ' cleaned..')
